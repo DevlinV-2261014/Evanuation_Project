7c17871cc0c8594731d2180c74764a3b7f4caee2
DevinP-1746830 <devin.pelckmans@student.uhasselt.be>
2024-05-14 15:44:35 +0200

    Fix - Navbar

--- src/components/kanbanboard/kanbanboard.js
+++ src/components/kanbanboard/kanbanboard.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './kanbanboard.css';
-import Nav from '../Nav/Nav';
 
 const initialTasks = {
   'To Do': [{ title: 'Task 1', description: 'Description 1' }, { title: 'Task 2', description: 'Description 2' }],
@@ -13,23 +12,20 @@ const KanbanBoard = () => {
   const [tasks, setTasks] = useState(initialTasks);
 
   return (
-    <div className="test">
-      <Nav />
-      <div className="kanban-board">
-        {Object.entries(tasks).map(([column, tasks], index) => (
-          <div key={index} className="kanban-column">
-            <h2>{column}</h2>
-            <div className="kanban-cards">
-              {tasks.map((task, index) => (
-                <div key={index} className="kanban-card">
-                  <h3>{task.title}</h3>
-                  <p>{task.description}</p>
-                </div>
-              ))}
-            </div>
+    <div className="kanban-board">
+      {Object.entries(tasks).map(([column, tasks], index) => (
+        <div key={index} className="kanban-column">
+          <h2>{column}</h2>
+          <div className="kanban-cards">
+            {tasks.map((task, index) => (
+              <div key={index} className="kanban-card">
+                <h3>{task.title}</h3>
+                <p>{task.description}</p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
