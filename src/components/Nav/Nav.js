f1bc4ed041703c4119d0e65c225d0f3eadf54e00
DevinP-1746830 <devin.pelckmans@student.uhasselt.be>
2024-05-14 15:32:11 +0200

    Add - Navbar component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import "./Nav.css";
+
+function Nav() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-left">
+                <img src={logo} alt="Company Logo" className="logo" />
+                <span className="company-title">Kanban Board</span>
+            </div>
+            <ul className="navbar-links">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">About</a>
+                </li>
+                <li>
+                    <a href="/">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
